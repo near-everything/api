437d978e4cc70bdb4bce170544eca40c180170f2
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2022-06-03 12:03:46 -0400

    feat: adds create, edit, delete

--- app.py
+++ app.py
@@ -23,3 +23,43 @@ class Category(db.Model):
 def index():
     categories = Category.query.all()
     return render_template('index.html', categories=categories)
+
+@app.route('/<int:category_id>/')
+def category(category_id):
+    category = Category.query.get_or_404(category_id)
+    return render_template('category.html', category=category)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        category = Category(name=name)
+        db.session.add(category)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+@app.route('/<int:category_id>/edit/', methods=('GET', 'POST'))
+def edit(category_id):
+    category = Category.query.get_or_404(category_id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+
+        category.name = name
+
+        db.session.add(category)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', category=category)
+
+@app.post('/<int:category_id>/delete/')
+def delete(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for('index'))
